a24d21722e2e0205b5ca471f00bc15241a5d7795
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 13:32:14 +0300

    feat: added new builds

--- app/main/views.py
+++ app/main/views.py
@@ -1,5 +1,5 @@
 from flask import render_template,request,redirect,url_for
-from app import app
+from . import main
 from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
 from ..models import review
@@ -9,7 +9,7 @@ Review = review.Review
 # Views
 
 
-@app.route('/')
+@main.route('/')
 def index():
     '''
     View root page function that returns the index page and its data
@@ -25,12 +25,12 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('.search',movie_name=search_movie))
     else:
         return render_template('index.html', title=title, popular=popular_movies, upcoming=upcoming_movie, now_showing=now_showing_movie)
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
     '''
     View movie page function that returns the movie details page and its data
@@ -43,7 +43,7 @@ def movie(id):
     return render_template('movie.html',title = title, movie=movie, reviews=reviews)
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to diplay the search results
@@ -55,7 +55,7 @@ def search(movie_name):
     title = f'search results for {movie_name}'
     return render_template('search.html',movies = searched_movies)
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -67,7 +67,7 @@ def new_review(id):
         new_review = Review(movie.id,title,movie.poster,review)
         new_review.save_review()
         
-        return redirect(url_for('movie',id = movie.id ))
+        return redirect(url_for('.movie',id = movie.id ))
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
