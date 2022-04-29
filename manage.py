859b9d8c6ad3265ee96f2a30d88d32b1a46e4695
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 13:37:24 +0300

    build: imported functionality and renamed files

---
+++ manage.py
@@ -0,0 +1,11 @@
+from app import create_app
+from flask_script import Manager,Server
+
+# Create an app instance
+app = create_app('development')
+
+manager = Manager(app)
+manager.add_command('server',Server)
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=5003, debug=True)
