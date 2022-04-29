a3e7d49d2426ec6afff28887fa5afa77a411673a
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 18:10:19 +0300

    chore: updated code with testsource

--- manage.py
+++ manage.py
@@ -1,17 +1,20 @@
+from doctest import testsource
 from app import create_app
-from flask_script import Manager,Server
+from flask_script import Manager,Shell,Server
 
 # Create an app instance
 app = create_app('development')
 
 manager = Manager(app)
+
 manager.add_command('server',Server)
+
 @manager.command
 def test():
     """Run the unit tests."""
     import unittest
     tests = unittest.TestLoader().discover('tests')
-    unittest.TextTestRunner(verbosity=2).run(tests)
+    unittest.TextTestRunner(verbosity=2).run(testsource)
 
 if __name__ == '__main__':
     manager.run()
