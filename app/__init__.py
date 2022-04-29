9b342448cd715c7f0e69bafa67fa648af75745d4
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 11:53:34 +0300

    feat: added a new function to get config_options in config file

--- app/__init__.py
+++ app/__init__.py
@@ -1,16 +1,23 @@
 from flask import Flask
 from flask_bootstrap import Bootstrap
-from .config import DevConfig
-
-# Initializing application
-app = Flask(__name__,instance_relative_config = True)
-
-# Setting up configuration
-app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
+from config import config_options
 
 # Initializing Flask Extensions
-bootstrap = Bootstrap(app)
+bootstrap = Bootstrap()
+
+def create_app(config_name):
+    
+    app = Flask(__name__)
+
+    # Creating the app configurations
+    app.config.from_object(config_options[config_name])
+
+    # Initializing flask extensions
+    bootstrap.init_app(app)
+
+    # Will add the views and forms
+
+    return app
 
 from app import views
 from app import error
