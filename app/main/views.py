ac8e3f5ff0c461e352226128395f0be67bb203f1
kelvinmburu <mburuhkelvin@gmail.com>
2022-04-29 18:08:05 +0300

    refactor: fixed code typo

--- app/main/views.py
+++ app/main/views.py
@@ -2,7 +2,7 @@ from flask import render_template,request,redirect,url_for
 from . import main
 from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-from ..models import review
+from ..models import Review
 
 # Views
 
