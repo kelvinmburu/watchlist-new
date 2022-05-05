619db194a32fdec04bb62fd97d7c92be5bb3c1ad
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-05 11:19:21 +0300

    feat: added sqlalchemy access configuration

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ class Config:
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key=21301c876ea97d48088604bdcfd72107'
     MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
-    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://kelvin:mburu95@localhost/watchlist'
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://postgres:Access@localhost/watchlist'
     
     @staticmethod
     def init_app(app):
