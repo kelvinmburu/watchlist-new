40560d279d35a49608b91160087129490e072906
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-08 17:54:27 +0300

    feat: build requirements

--- app/main/forms.py
+++ app/main/forms.py
@@ -7,3 +7,7 @@ class ReviewForm(FlaskForm):
     title = StringField('Review title',validators=[DataRequired()])
     review = TextAreaField('Movie review', validators=[DataRequired()])
     submit = SubmitField('Submit')
+    
+class UpdateProfile(FlaskForm):
+    bio = TextAreaField('Tell us about you.',validators=[DataRequired()])
+    submit = SubmitField('Submit')
