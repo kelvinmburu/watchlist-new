54425490ff43ec985748e454ce906c3807008015
kelvinmburu <mburuhkelvin@gmail.com>
2022-05-08 17:41:00 +0300

    build: updated file changes

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,9 @@
-from flask import render_template,request,redirect,url_for
+from flask import render_template,request,redirect,url_for,abort
 from . import main
 from ..request import get_movies,get_movie,search_movie
 from .forms import ReviewForm
-from ..models import Review
+from ..models import Review,User
+from flask_login import login_required
 
 # Views
 
@@ -54,6 +55,7 @@ def search(movie_name):
     return render_template('search.html',movies = searched_movies)
 
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -69,4 +71,13 @@ def new_review(id):
 
     title = f'{movie.title} review'
     return render_template('new_review.html',title = title, review_form=form, movie=movie)
+
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+    
+    if user is None:
+        abort(404)
+        
+    return render_template('profile/profile.html', user=user)
 
--- app/models.py
+++ app/models.py
@@ -1,5 +1,11 @@
 from . import db
 from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
+from . import login_manager
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 class Movie:
     '''
@@ -46,12 +52,16 @@ class Review:
 
         return response
     
-class User(db.Model):
+class User(UserMixin,db.Model):
     __tablename__ = 'users'
+    
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(255))
+    email = db.Column(db.String(255),unique = True,index = True)
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
-    pass_secure = db.Column(db.String(255))
+    bio = db.Column(db.String(255))
+    profile_pic_path = db.Column(db.String())
+    password_secure = db.Column(db.String(255))
     
     def __repr__(self):
         return f'User {self.username}'
@@ -79,3 +89,4 @@ class Role(db.Model):
 
     def verify_password(self,password):
             return check_password_hash(self.pass_secure,password)
+        
